e034d1eece5c29e01eedf1e2e0b2485f5608d90c
Jamie Vos <Jamievos100@gmail.com>
2026-02-02 11:52:02 +0100

    feat: node.js, liquid en express toegevoegd aan mijn profilecard

---
+++ public/scripts/script.js
@@ -0,0 +1,9 @@
+// Zoek de button met de class .alert
+let button = document.querySelector('.alert');
+
+// Als je over de button hovert, doe dan een alert
+button.addEventListener('mouseover', function(){
+    alert("Mediacollege Amsterdam")
+  }
+)
+
